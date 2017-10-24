9a5e1f6650c30eb034e46d48f09ab9973aa8db10
john magann <jmagann28@gmail.com>
2017-10-24 14:01:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+var musicians = ['John Lennon', 'George Harrison', 'Paul McCartney', 'Ringo Starr']
+var instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums']
+
+function theBeatlesPlay(){
+var strings = []
+
+}
